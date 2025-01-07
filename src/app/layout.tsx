f99c816a5875cd0ff609b2b1bb535ddd940e83ed
Vitalii S. <svitalii138@gmail.com>
2025-01-07 17:58:24 +0200

    upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,24 +13,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <Head>
-        {/* Favicon */}
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
-        {/* Preconnect */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
-        {/* Alternate Languages */}
         <link rel="alternate" hrefLang="x-default" href="https://neurogenlab.de/" />
         <link rel="alternate" hrefLang="en" href="https://neurogenlab.de/en/" />
         <link rel="alternate" hrefLang="de" href="https://neurogenlab.de/" />
       </Head>
       <body>
-        {/* Google Tag Manager */}
         <Script
           id="google-tag-manager"
           strategy="afterInteractive"
