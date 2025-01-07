1a3c7588ac3c1bd927c50130139657bc6ddf2bcd
Vitalii S. <svitalii138@gmail.com>
2025-01-07 16:38:19 +0200

    RootLayout created

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from "react";
+import Head from "next/head";
+import Script from "next/script";
+import "../styles/index.css";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <Head>
+        {/* Metadata */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Your site description" />
+
+        {/* Favicon */}
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Preconnect */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        {/* Alternate Languages */}
+        <link rel="alternate" hrefLang="x-default" href="https://neurogenlab.de/" />
+        <link rel="alternate" hrefLang="en" href="https://neurogenlab.de/en/" />
+        <link rel="alternate" hrefLang="de" href="https://neurogenlab.de/" />
+      </Head>
+      <body>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MZNC2SFX');
+            `,
+          }}
+        />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZNC2SFX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
