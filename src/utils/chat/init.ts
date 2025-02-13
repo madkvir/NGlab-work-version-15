b3739efbb15f5dc5a00297ae29ee54bcbd30c97a
Vitalii S. <svitalii138@gmail.com>
2025-02-13 17:22:39 +0200

    widget fixed

--- src/utils/chat/init.ts
+++ src/utils/chat/init.ts
@@ -1,22 +1,22 @@
-import type { WindowWithChat } from './types';
-import { DEFAULT_CONFIG } from './config';
-import { loadScript } from '../script-loader/load';
-import { handleChatWidgetError } from './error-handlers';
-import { CHAT_WIDGET_URL } from './constants';
+import type { WindowWithChat } from "./types";
+import { DEFAULT_CONFIG } from "./config";
+import { loadScript } from "../script-loader/load";
+import { handleChatWidgetError } from "./error-handlers";
+import { CHAT_WIDGET_URL } from "./constants";
 
 export const initChatWidget = async (): Promise<void> => {
   const win = window as WindowWithChat;
-  
-  // Only initialize if not already loaded
-  if (win.zappr) {
-    return;
-  }
+
+  // // Only initialize if not already loaded
+  // if (win.zappr) {
+  //   return;
+  // }
 
   try {
     // Set up initialization callback
     win.zapprLoaded = () => {
       try {
-        win.zappr?.setPosition('bottom-right');
+        win.zappr?.setPosition("bottom-right");
       } catch (error) {
         handleChatWidgetError(error as Error);
       }
@@ -29,7 +29,7 @@ export const initChatWidget = async (): Promise<void> => {
     await loadScript(CHAT_WIDGET_URL, {
       async: true,
       defer: true,
-      id: 'zappr-widget-script'
+      id: "zappr-widget-script",
     });
   } catch (error) {
     handleChatWidgetError(error as Error);
